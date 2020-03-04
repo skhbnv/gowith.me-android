17c3d1691ad2e237e664cfeb76ac175e8f5e64af
unknown <skhbnv@gmail.com>
2020-03-04 22:21:15 +0600

    reading from json locally is in progress

--- app/src/main/java/com/example/gowithme/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/gowithme/ui/dashboard/DashboardFragment.kt
@@ -6,16 +6,19 @@ import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.gowithme.MainActivity
 import com.example.gowithme.R
 import com.example.gowithme.network.ApiRepository
-import com.example.gowithme.network.RetrofitApi
-import kotlinx.android.synthetic.main.fragment_dashboard.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+
 
 class DashboardFragment : Fragment() {
 
@@ -27,14 +30,26 @@ class DashboardFragment : Fragment() {
     private var recyclerView: RecyclerView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?): View? {
+        observeTheFields()
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    private fun observeTheFields() {
+        dashboardViewModel.events.observe(viewLifecycleOwner, Observer { listOfEvents ->
+            adapter?.initTheList(listOfEvents)
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecycler()
-        makeRequest()
+//        makeRequest()
+        getLocally()
+    }
+
+    private fun getLocally() {
+        loadJsonFromAsset()
     }
 
     private fun initRecycler() {
@@ -42,17 +57,38 @@ class DashboardFragment : Fragment() {
             _onClick = {
                 d("___", "sadasd")
             },
-            _context = (activity as Context))
+            _context = (activity as Context)
+        )
         recyclerView = view?.findViewById(R.id.recycler)
         recyclerView?.adapter = adapter
         recyclerView?.layoutManager = LinearLayoutManager(activity)
     }
 
-    private fun makeRequest() {
-        dashboardViewModel.events.observe(this, Observer {listOfEvents ->
-            adapter?.initTheList(listOfEvents)
-        })
+    private fun makeRequest() = dashboardViewModel.getEvents()
 
-        dashboardViewModel.getEvents()
+    fun loadJsonFromAsset(): String? {
+        var json: String? = null
+        try {
+            val inst = (activity as MainActivity).assets.open("a.json")
+
+            val size = inst.available()
+            val buffer = ByteArray(size)
+            inst.read(buffer)
+            inst.close()
+
+            json = String(buffer)
+
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        }
+
+//        try {
+//            val inputStream: InputStream = context!!.assets.open("a.json")
+//            json = inputStream.bufferedReader().use { it.readText() }
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+
+        return json
     }
 }
