514e4045feffac3cd7f6dcaf41cf1895a890fe06
Madi <skhbnv@gmail.com>
2020-03-25 00:18:53 +0600

    images changed to String

--- app/src/main/java/com/example/gowithme/MainActivity.kt
+++ app/src/main/java/com/example/gowithme/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_profile
             )
         )
+//        sd
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
