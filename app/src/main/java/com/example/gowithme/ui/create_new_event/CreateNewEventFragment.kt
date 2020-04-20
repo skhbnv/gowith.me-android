b5467d9ba657f40303e010a25ce9b57bd65ac46e
Kuanysh <4kuanish@gmail.com>
2020-04-20 15:36:26 +0600

    create new event layout init

--- app/src/main/java/com/example/gowithme/ui/create_new_event/CreateNewEventFragment.kt
+++ app/src/main/java/com/example/gowithme/ui/create_new_event/CreateNewEventFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.gowithme.MainActivity
 import com.example.gowithme.R
+import com.example.gowithme.databinding.FragmentCreateNewEventBinding
+import com.example.gowithme.util.showToast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -17,6 +21,7 @@ class CreateNewEventFragment : Fragment() {
     private val mainActivityInstance by lazy {
         (activity as MainActivity?)
     }
+    private lateinit var binding: FragmentCreateNewEventBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +33,16 @@ class CreateNewEventFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create_new_event, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_create_new_event, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(binding) {
+            startDateInput.setOnClickListener {
+
+            }
+        }
     }
 }
